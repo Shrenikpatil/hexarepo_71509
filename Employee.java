7d06ee0defa59c23443f40ff3a77a11511a5ac2a
Shrenikpatil <89975357+Shrenikpatil@users.noreply.github.com>
2022-05-02 15:27:13 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,19 @@
+public class Employee
+{
+   private int empid ;
+   private String ename;
+   private double salary;
+   public Employee(){}
+   public Employee(int empid,String ename,double salary)
+   {
+   this.empid=empid;
+     this.ename=ename;
+     this.salary=salary;
+   }
+  public String tostring()
+  {
+     return empid+"  "+ename+"  "+salary;
+    
+  }
+  public boolean equals(0);
+}
