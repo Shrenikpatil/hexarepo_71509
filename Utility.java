731b21fea530cb9234ba0d10cfefb8cfaae83636
Shrenikpatil <89975357+Shrenikpatil@users.noreply.github.com>
2022-05-02 14:54:33 +0530

    Add files via upload

---
+++ Utility.java
@@ -0,0 +1,22 @@
+package shapes;
+public class Utility  
+{
+           public static int sum(int x,int y)
+             {
+                 return x + y;
+              }
+            public static int square(int x)
+             {
+                 return x * x;
+              }
+            public static int sum(int x,int n)
+             {   
+                 int res =1;
+                 for (;n>0;n--)
+                    res =res* x
+              return res;
+                
+              }
+}
+
+
